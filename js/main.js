baad9ed14ffbd960d81c84cc67ae6381f2fa8cb4
Joshua Colon <joshuacolon1992@gmail.com>
2014-08-14 16:57:29 +0000

    AddSpacejump

--- js/main.js
+++ js/main.js
@@ -6,34 +6,73 @@ var game = new Phaser.Game(400,490, Phaser.AUTO,"gameDiv");
 //This is the body of the game Itself
 //It should contain all code related to the game itself
 
-var mainState = { 
+var mainState = {
 
  preload:function () {
     //This function will execute at the beginning 
     //Which is where we'll load our assets for the game
     
     //Set the background color of the game
-    game.stage.backgroundColor = "#71c5cf"; 
+    game.stage.backgroundColor = "#71c5cf";
+    
+    game.load.image("bird", "Assets/bird.png");
+    
+    game.load.image("pipe", "Assets/pipe.png");
+    
+    
+},    
 
 
-},
-
-
-  create:function () {
+ create:function () {
     //This function is called right after preload function ()
     //This is where we set up the game assets from earlier
+    
+    game.physics.startSystem(Phaser.Physics.Arcade);
+
+    this.bird = this.game.add.sprite(100, 245, "bird");
+
+
+    //Now that we have a bird and gravity...we need to tell the bird
+    //to react to the gravity
+    
+    game.physics.arcade.enable(this.bird);
+    
+    this.bird.body.gravity.y = 1000;
+    
+    var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    
+    spaceKey.onDown.add(this.jump, this);
 },
 
 update: function () {
   //This function runs 60 times per second
+  
+  
+  //Check if the bird is outside of the gamescreen
+  if (this.bird.inWorld == false) {
+   this.restartGame();
+   }
+
+
 },
-  
-}
 
 
-//Add and start the "mainState" ro start the game
-game.state.add("main", mainState)
-game.state.start("main");
+ jump: function () {
+   
+   //Let's make our bird jump!
+   this.bird.body.velocity.y = -350;
+ },
+ 
+ restartGame: function () {
+  game.state.start("main");  
+ },
+ 
+ 
+ };
+ 
+ //Add and start the "mainState" to start the game
+game.state.add("main", mainState);
+game.state.start("main"); 
+
   
-}
-  }
+
